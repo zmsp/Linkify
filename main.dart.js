a945fd5ced41387227db59330487e9187bdce421
zmsp <zmsp@users.noreply.github.com>
2026-07-21 08:13:53 +0000

    Automated deploy [e985b1ef180313e906ea04127fe43bcf400a1885]

--- main.dart.js
+++ main.dart.js
@@ -30195,7 +30195,7 @@ $S:2}
 A.aA4.prototype={
 $1(a){var s=A.dt().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/a10d8ac38de835021c8d2f920dbf50a920ccc030/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/69c8c61792f04cc809dfef0c910414fb9afc06cd/":s)+a},
 $S:43}
 A.ON.prototype={
 gA(a){var s=this.a
