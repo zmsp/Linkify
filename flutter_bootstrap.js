c0006183f1a4d723c802c39da32a18aea174da4d
zmsp <zmsp@users.noreply.github.com>
2026-08-11 23:08:36 +0000

    Automated deploy [142cb3cb80ba1c3fa21f4a8998906ed0f6befc35]

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06cd","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624aa1","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3091610432" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1896510635" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
